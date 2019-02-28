efed3233fc3520e61105f663d85abf6827a47e4c
jonasryen <33313947+jonasryen@users.noreply.github.com>
2019-02-28 09:11:50 +0100

    Main file 0.1
    Only funtion declarations

---
+++ neuralnetwork.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+
+/*declaring all functions */
+void read_data();
+void function(); 
+void input_layer();
+void error(); 
+void learning();
+void epoch(); 
+void initialize(); 
+
+/*running the program*/
+int main() {
+printf("I'm a neural network \n");
+}
+
+/*writing all functions */
