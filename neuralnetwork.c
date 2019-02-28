60047c7d084b3b2b34a759640b966f5475060a6d
Oskar Svendsen <onsvends@stud.ntnu.no>
2019-02-28 10:17:30 +0100

    Add sigmoid function

--- neuralnetwork.c
+++ neuralnetwork.c
@@ -11,9 +11,20 @@ void learning();
 void epoch(); 
 void initialize(); 
 
+/* Different functions used by nodes in network */
+
+/*Sigmoid function*/
+
+double Sigmoid(double in) {
+	return(1.0/(1.0 + exp(-in)));
+
+}
+
+
 /*running the program*/
 int main() {
 printf("I'm a neural network \n");
+printf("%f\n",Sigmoid(0.5));
 }
 
 /*writing all functions */
